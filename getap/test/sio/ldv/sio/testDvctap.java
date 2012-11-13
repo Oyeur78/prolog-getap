b120b0dda22eac20957a08ae28d517cf686aa38c
Oyeur78 <victor.varadaradjou@laposte.net>
2012-11-13 11:32:45 +0100

    - Test user

---
+++ getap/test/sio/ldv/sio/testDvctap.java
@@ -0,0 +1,18 @@
+package sio.ldv.sio;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class testDvctap {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUser() {
+
+	}
+}
