3558c45ab25a8f14c64088a2ffb7d124a452d672
Oyeur78 <victor.varadaradjou@laposte.net>
2012-11-16 13:11:45 +0100

    - Réaliastion des test unitaires de la classe DemandeValidationConsoTempsAccPers

--- getap/test/sio/ldv/sio/testDvctap.java
+++ getap/test/sio/ldv/sio/testDvctap.java
@@ -1,18 +1,92 @@
 package sio.ldv.sio;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.sql.Date;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.ldv.sio.getap.app.AccPersonalise;
+import org.ldv.sio.getap.app.Classe;
+import org.ldv.sio.getap.app.DVCTAPException;
+import org.ldv.sio.getap.app.DemandeValidationConsoTempsAccPers;
+import org.ldv.sio.getap.app.User;
 
 public class testDvctap {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private DemandeValidationConsoTempsAccPers dvctap;
+
+	@Before
+	public void setUp() {
+		Classe classe = new Classe(0, "SIO SLAM");
+		User prof = new User(0l, "Varadaradjou", "Victor", classe, "profPrin");
+		User eleve = new User(1l, "Alexandre", "Soler", classe, "eleve");
+		AccPersonalise accPers = new AccPersonalise(0, "Salon du libre", 0, 0l);
+		Date date = Date.valueOf("2012-12-16");
+		dvctap = new DemandeValidationConsoTempsAccPers(0l, "2012-2013", date,
+				240, prof, accPers, eleve, 0);
 	}
 
 	@Test
-	public void testUser() {
+	public void testEtatInitial() {
+		try {
+			assertTrue("Etat initial", dvctap.isEtatInitial());
+		} catch (DVCTAPException e) {
+			System.out.println(e);
+		}
 
 	}
+
+	@Test
+	public void testModifieeEleve() {
+		try {
+			dvctap.modifieeParEleve();
+			assertTrue("Etat modifié élève", dvctap.getEtat() == 4);
+		} catch (DVCTAPException e) {
+			System.out.println(e);
+		}
+	}
+
+	@Test
+	public void testAnnulerEleve() {
+		try {
+			dvctap.rejeteParEleveApresModProf();
+			assertTrue("Etat annulé par l'élève", dvctap.getEtat() == 2);
+		} catch (DVCTAPException e) {
+			System.out.println(e);
+		}
+	}
+
+	@Test
+	public void testValiderProfesseur() {
+		try {
+			dvctap.setEtat(0);
+			dvctap.valideeParLeProfesseur();
+			dvctap.modifieeParEleve();
+			fail("Etat modifié élève");
+		} catch (DVCTAPException e) {
+			System.out.println(e);
+			assertTrue(1 == 1);
+		}
+	}
+
+	@Test
+	public void testDiffPassageEtat() {
+		try {
+			dvctap.setEtat(0);
+			dvctap.modifieeParEleve();
+			dvctap.valideeParLeProfesseur();
+			dvctap.modifieeParEleve();
+			dvctap.dureeModifieeParLeProfesseur();
+			dvctap.rejeteeParEleve();
+			dvctap.valideeParLeProfesseur();
+			System.out.println(dvctap.getEtat());
+			fail("Etat modifié élève");
+		} catch (DVCTAPException e) {
+			System.out.println(e);
+			assertTrue(1 == 1);
+		}
+	}
+
 }
